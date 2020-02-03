dc0f5ed250b375fce9224e6d8be77db1cc88093f
pjchender <pjchender@gmail.com>
2020-02-03 15:42:42 +0800

    fix: add rollup-plugin-terser for minifing bundle files

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/index.js',
     output: [
+      {
+        file: pkg.main.replace('.min', ''),
+        format: 'cjs',
+        sourcemap: true,
+      },
       {
         file: pkg.main,
         format: 'cjs',
@@ -20,6 +26,9 @@ export default [
     plugins: [
       resolve(), // so Rollup can find `ms`
       commonjs(), // so Rollup can convert `ms` to an ES module
+      terser({
+        include: [/^.+\.min\.js$/],
+      }),
     ],
   },
 ];
